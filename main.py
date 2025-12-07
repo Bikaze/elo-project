2b15f0029186cab3dd8a69180ec5bf303f57c7b9
Bikaze <clmntmugisha@gmail.com>
2025-12-07 14:14:28 +0200

    Add data transformation functionality and update constants

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import (
@@ -26,6 +27,12 @@ if __name__ == "__main__":
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        logging.info("Initiate the Data Transformation")
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation Completed")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
